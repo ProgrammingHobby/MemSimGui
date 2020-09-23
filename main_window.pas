6ba2b0cd738394ab20704a39b988058cd3c969f0
ProgrammingHobby <Uwe.Merker@KabelBW.de>
2020-09-23 18:09:42 +0200

    Menüstruktur angelegt

--- main_window.pas
+++ main_window.pas
@@ -5,10 +5,47 @@ unit Main_Window;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, ActnList;
 
 type
+
+  { TMainWindow }
+
   TMainWindow = class(TForm)
+    actionAbout: TAction;
+    actionResetDuration: TAction;
+    actionResetImpulse: TAction;
+    actionEmulationOn: TAction;
+    actionUploadReset: TAction;
+    actionUpload: TAction;
+    actionEpromType: TAction;
+    actionCopy: TAction;
+    actionFill: TAction;
+    actionErase: TAction;
+    actionQuit: TAction;
+    actionOpen: TAction;
+    ActionList: TActionList;
+    MainMenu: TMainMenu;
+    menuFile: TMenuItem;
+    menuEdit: TMenuItem;
+    MenuItem1: TMenuItem;
+    menuErase: TMenuItem;
+    menuFill: TMenuItem;
+    menuCopy: TMenuItem;
+    menuEmulationOn: TMenuItem;
+    menuEpromType: TMenuItem;
+    menuAbout: TMenuItem;
+    menuResetDuration: TMenuItem;
+    menuResetImpluse: TMenuItem;
+    N4: TMenuItem;
+    N3: TMenuItem;
+    N2: TMenuItem;
+    menuUploadReset: TMenuItem;
+    menuUpload: TMenuItem;
+    menuQuit: TMenuItem;
+    N1: TMenuItem;
+    menuOpen: TMenuItem;
+    menuSimulator: TMenuItem;
   private
 
   public
